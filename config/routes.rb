0c1caef5e0dde58e16c6862ee3b52466394951c6
tomgdow <thomasgdowling@gmail.com>
2016-11-12 02:47:01 +0000

    scaffold with fruit and car scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :products do
+		collection do
+			get 'fruit', defaults: {format: :json}
+			get 'car', defaults: {format: :json}
+		end
+	end
 end
